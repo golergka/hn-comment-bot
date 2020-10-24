a434637ffee20c7c31586a8c5c6518ccc4e6d166
Max <golergka@gmail.com>
2020-10-24 16:07:13 +0300

    Basic crawler

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,36 @@
-describe('Test', () => {
-	it.concurrent('passes', async () => {
-		expect(1).toEqual(1)
+import { Root, ItemID, User, Item, IDBReadable, HN, Crawler } from './index'
+import { mock, instance, when, anything } from 'ts-mockito'
+
+describe('getNewCommentsForRoot', () => {
+	it.concurrent('returns new item', async () => {
+		const dbMock = mock<IDBReadable>()
+		when(dbMock.getUser("golergka")).thenResolve({ id: "golergka", submitted: ['24857357'] })
+		when(dbMock.getRoot('24857357')).thenResolve({ id: "24857357", kids: ['24857727']})
+		const hnMock = mock<HN>()
+		when(hnMock.getUser('golergka')).thenResolve({ id: "golergka", submitted: ['24857357'] })
+		when(hnMock.getItem('24857357')).thenResolve({
+			id: '24857357',
+			parent: '24857242',
+			kids: ['24857727', '24858118'],
+			by: 'golergka',
+			text:
+				'&gt; XYZ’s protocol is not just based on JSON, but it’s based on a particular technique known as polymorphic JSON. In this, a single field could have a different data type in different circumstances. For example, the resources field can be an array when requesting a single access token, or it can be an object when requesting multiple named access tokens. The difference in type indicates a difference in processing from the server. Within the resources array, each element can be either an object, representing a rich resource request description, or a string, representing a resource handle (scope).<p>This is horrible.'
+		})
+		when(hnMock.getItem('24858118')).thenResolve({
+			id: '24858118',
+			parent: '24857357',
+			by: 'alyandon',
+			text: 'Lovely - I can&#x27;t wait to see how horrible something like that is to implement in a language like Go.'
+		})
+		const crawler = new Crawler(instance(hnMock), instance(dbMock))
+
+		const newItems = await crawler.getNewCommentsForRoot("24857357")
+
+		expect(newItems).toEqual([{
+			id: '24858118',
+			parent: '24857357',
+			by: 'alyandon',
+			text: 'Lovely - I can&#x27;t wait to see how horrible something like that is to implement in a language like Go.'
+		}])
 	})
 })
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,83 @@
-import { initializeApp, credential } from 'firebase-admin'
+import { difference, concat } from 'lodash'
+import got from 'got'
 
-initializeApp({
-	credential: credential.applicationDefault(),
-	databaseURL: 'https://hn-comment-bot.firebaseio.com'
-})
+export type ItemID = string
 
-console.log('Firebase initialised')
+export interface Root {
+	id: ItemID
+	kids?: ItemID[]
+}
+
+export interface User {
+	id: string
+	submitted?: ItemID[]
+}
+
+export interface IDBReadable {
+	getUser(id: string): Promise<User>
+	getRoot(id: ItemID): Promise<Root>
+}
+
+export interface Item {
+	id: ItemID
+	parent: ItemID
+	by: string
+	// title?: string
+	text?: string
+	kids?: ItemID[]
+	// type: "job"|"story"|"comment"|"poll"|"pollopt"
+}
+
+export interface HN {
+	getUser(id: string): Promise<User>
+	getItem(id: ItemID): Promise<Item>
+}
+
+export class Crawler {
+	constructor(readonly hn: HN, readonly db: IDBReadable) {}
+
+	public async getNewRootsForUser(dbUser: User): Promise<Root[]> {
+		const hnUser = await this.hn.getUser(dbUser.id)
+		const newRootIDs = difference(hnUser.submitted, dbUser.submitted || [])
+		return Promise.all(newRootIDs.map(this.hn.getItem))
+	}
+
+	async getNewCommentsForRoot(rootID: ItemID): Promise<Item[]> {
+		const [dbRoot, hnRoot] = await Promise.all([
+			this.db.getRoot(rootID),
+			this.hn.getItem(rootID)
+		])
+		if (!dbRoot.kids) {
+			return []
+		}
+		const newItems = difference(hnRoot.kids, dbRoot.kids)
+		return await Promise.all(newItems.map((n) => this.hn.getItem(n)))
+	}
+
+	public async getNewCommentsForUser(dbUser: User): Promise<Item[]> {
+		if (!dbUser.submitted) {
+			return []
+		}
+		const newComments = await Promise.all(
+			dbUser.submitted.map((r) => this.getNewCommentsForRoot(r))
+		)
+		return concat([], ...newComments)
+	}
+}
+
+async function getItem(id: ItemID) {
+	const { body } = await got<Item>(
+		`https://hacker-news.firebaseio.com/v0/item/${id}.json`,
+		{
+			responseType: 'json'
+		}
+	)
+	return body
+}
+
+;(async () => {
+	/*
+	const item = await traverseItem('24857357')
+	console.log('response', JSON.stringify(item))
+	*/
+})()
