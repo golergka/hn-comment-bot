ca7b091ddb16c32cf24f4f71d2419f455ff54261
Max <golergka@gmail.com>
2020-10-29 02:00:44 +0300

    Primitive work queue

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import { concat, chunk } from 'lodash'
+import { concat, chunk, groupBy } from 'lodash'
 import got from 'got'
 import { sql } from '@pgtyped/query'
 import {
 	ICreateHnUserQuery,
 	ICreateKidsQuery,
 	ICreateRootsQuery,
+	ICreateSubscriptionQuery,
+	IDeleteSubscriptionQuery,
+	IGetAllSubscriptionsQuery,
 	IGetHnUserQuery,
 	IGetSessionQuery,
+	IGetSubscribedUsersQuery,
+	IGetSubscriptionsByUserQuery,
+	IGetUnnotifiedPostsQuery,
+	IMarkOutdatedPostsQuery,
+	ISetPostsNotifiedQuery,
 	ISetSessionQuery
 } from './index.types'
 import { pg, tx } from './pg'
@@ -14,6 +22,8 @@ import Telegraf from 'telegraf'
 import { TelegrafContext } from 'telegraf/typings/context'
 import { PoolClient } from 'pg'
 import { DateTime } from 'luxon'
+import { Chat } from 'telegraf/typings/telegram-types'
+import { promisify } from 'util'
 
 export interface Item {
 	id: ItemID
@@ -29,14 +39,14 @@ export interface Item {
 export type ItemID = number
 
 const createRoots = sql<ICreateRootsQuery>`
-	INSERT INTO hn_submitted (hn_username, hn_id)
+	INSERT INTO hn_submitted (hn_user_id, id)
 	VALUES $$roots(hnUsername, id)
-	ON CONFLICT (hn_id) DO NOTHING
+	ON CONFLICT (id) DO NOTHING
 `
 
 const createKids = sql<ICreateKidsQuery>`
-	INSERT INTO hn_kids (parent_id, id)
-	VALUES $$kids(parentId, id)
+	INSERT INTO hn_kids (parent_id, id, posted_at)
+	VALUES $$kids(parentId, id, postedAt)
 	ON CONFLICT (id) DO NOTHING
 	RETURNING parent_id, id
 `
@@ -60,77 +70,50 @@ async function loadHNUser(username: string): Promise<{ submitted?: ItemID[] }> {
 	return body
 }
 
-async function updateRoots(
-	hnUsername: string,
-	subscribedAt: DateTime,
-	chatIds: number[],
-	submitted: number[]
-): Promise<void> {
+async function updateKids(submitted: number[]): Promise<void> {
 	const hnRoots = await Promise.all(submitted.map(loadHNItem))
-	const createdKids = await tx(async (db) => {
-		const [_, createdKids] = await Promise.all([
-			createRoots.run(
-				{
-					roots: hnRoots.map((r) => ({
-						hnUsername,
-						id: r.id
-					}))
-				},
-				db
-			),
-			createKids.run(
-				{
-					kids: concat(
-						[],
-						...hnRoots.map((r) =>
-							(r.kids || []).map((k) => ({
-								id: k,
-								parentId: r.id
-							}))
-						)
-					)
-				},
-				db
-			)
-		])
-		return createdKids
-	})
-	await Promise.all(
-		createdKids.map(async (k) => {
-			const kidItem = await loadHNItem(k.id)
-			const kidRoot = hnRoots.find((r) => r.id === k.parent_id)
-			const kidCreatedAt = DateTime.fromMillis(kidItem.time * 1000)
-			if (kidCreatedAt > subscribedAt) {
-				const url = `https://news.ycombinator.com/item?id=${kidItem.id}`
-				const notificationText = `${
-					kidItem.text
-				}\n[${kidCreatedAt.toLocaleString(DateTime.DATETIME_MED)} by ${
-					kidItem.by
-				}](${url})`
-				await Promise.all(
-					chatIds.map((c) => bot.telegram.sendMessage(c, notificationText))
-				)
-			}
-		})
+	const kids = concat(
+		[],
+		...hnRoots.map((r) =>
+			(r.kids || []).map((k) => ({
+				id: k,
+				parentId: r.id,
+				postedAt: new Date(r.time * 1000)
+			}))
+		)
 	)
+	if (kids.length > 0) {
+		await createKids.run(
+			{
+				kids
+			},
+			pg
+		)
+	}
 }
 
-async function updateUser(
+async function updateRoots(
 	hnUsername: string,
-	subscribedAt: DateTime,
-	chatIds: number[]
+	submitted: number[]
 ): Promise<void> {
-	const start = new Date().getTime()
+	console.log(`Checking posts ${JSON.stringify(submitted)}`)
+	await createRoots.run(
+		{
+			roots: submitted.map((id) => ({ hnUsername, id }))
+		},
+		pg
+	)
+	await updateKids(submitted)
+}
+
+async function updateUser(hnUsername: string): Promise<void> {
+	console.log(`Checking user ${hnUsername}`)
 	const hnUser: { submitted?: ItemID[] } = await loadHNUser(hnUsername)
 	const submitted = hnUser.submitted || []
 	const chunks = chunk(submitted, 10)
 
 	for (const c of chunks) {
-		await updateRoots(hnUsername, subscribedAt, chatIds, c)
-		/*
-		const now = new Date().getTime()
-		if ((now - start) >= 3000) { break }
-		*/
+		await updateRoots(hnUsername, c)
 	}
 }
 
@@ -142,23 +125,20 @@ if (!TELEGRAM_BOT_TOKEN) {
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN)
 
 const getSession = sql<IGetSessionQuery>`
-	SELECT session, hn_username
+	SELECT session
 	FROM tg_users
 	WHERE chat_id = $chatId
 	FOR UPDATE
 `
 
 const setSession = sql<ISetSessionQuery>`
-	INSERT INTO tg_users (chat_id, session, hn_username)
-	VALUES ($chatId, $session, $hnUsername)
-	ON CONFLICT (chat_id) DO UPDATE SET 
-		session = $session,
-		hn_username = $hnUsername
+	INSERT INTO tg_users (chat_id, session)
+	VALUES ($chatId, $session)
+	ON CONFLICT (chat_id) DO UPDATE SET session = $session
 `
 
 interface SessionContext extends TelegrafContext {
 	db?: PoolClient
-	hnUsername?: string
 	session?: any
 }
 
@@ -180,16 +160,11 @@ bot.use(async (ctx: SessionContext, next) => {
 	const chatId = ctx.chat.id
 	const result = await getSession.run({ chatId }, ctx.db!)
 	ctx.session = result[0]?.session || {}
-	ctx.hnUsername = result[0]?.hn_username || undefined
 
 	await next()
 
-	await setSession.run(
-		{ chatId, session: ctx.session, hnUsername: ctx.hnUsername },
-		ctx.db!
-	)
+	await setSession.run({ chatId, session: ctx.session }, ctx.db!)
 	ctx.session = undefined
-	ctx.hnUsername = undefined
 })
 
 // Logging chat details into db just in case
@@ -211,24 +186,56 @@ bot.start(({ reply }) =>
 )
 
 const getHNUser = sql<IGetHnUserQuery>`
-	SELECT hn_username, subscribed_at
+	SELECT id
 	FROM hn_users
-	WHERE hn_username = $hnUsername
+	WHERE id = $hnUsername
 `
 
 const createHNUser = sql<ICreateHnUserQuery>`
-	INSERT INTO hn_users (hn_username)
+	INSERT INTO hn_users (id)
 	VALUES ($hnUsername)
 `
 
+const createSubscription = sql<ICreateSubscriptionQuery>`
+	INSERT INTO tg_subscriptions (tg_user_chat_id, hn_user_id)
+	VALUES ($tgUserChatId, $hnUserId)
+	ON CONFLICT (tg_user_chat_id, hn_user_id) DO NOTHING
+	RETURNING *
+`
+
+const getSubscriptionsByUser = sql<IGetSubscriptionsByUserQuery>`
+	SELECT hn_user_id
+	FROM tg_subscriptions
+	WHERE tg_user_chat_id = $tgUserChatId
+`
+
+const getAllSubscriptions = sql<IGetAllSubscriptionsQuery>`
+	SELECT hn_user_id, tg_user_chat_id, subscribed_at
+	FROM tg_subscriptions
+`
+
+const deleteSubscription = sql<IDeleteSubscriptionQuery>`
+	DELETE
+	FROM tg_subscriptions
+	WHERE tg_user_chat_id = $tgUserChatId AND 
+		hn_user_id = $hnUserId
+	RETURNING *
+`
+
+function getArgument(ctx: SessionContext, index: number): string | undefined {
+	if (!ctx.message?.text) {
+		throw new Error(`can't get message text`)
+	}
+	const split = ctx.message.text.split(' ')
+	return split[index]
+}
+
 bot.command('subscribe', async (ctx: SessionContext) => {
-	const text = ctx.message!.text!
-	const split = text.split(' ')
-	if (split.length <= 1) {
+	const hnUsername = getArgument(ctx, 1)
+	if (!hnUsername) {
 		await ctx.reply('Please specify username to subscribe to')
 		return
 	}
-	const hnUsername = split[1]
 
 	const [check, _] = await Promise.all([
 		getHNUser.run({ hnUsername }, ctx.db!),
@@ -245,51 +252,175 @@ bot.command('subscribe', async (ctx: SessionContext) => {
 		await createHNUser.run({ hnUsername }, ctx.db!)
 	}
 
-	const oldUsername = ctx.hnUsername
-	ctx.hnUsername = hnUsername
-	const subscribeMessage = oldUsername
-		? `Subscribed to ${hnUsername} instead of ${oldUsername}. `
-		: `Subscriedb to ${hnUsername}. `
+	const [created] = await createSubscription.run(
+		{ tgUserChatId: ctx.session!.chat!.id, hnUserId: hnUsername },
+		ctx.db!
+	)
+	if (!created) {
+		await ctx.reply(
+			`You're already subscribed to ${hnUsername}! Use /subscriptions to check the list of existing subscriptions`
+		)
+		return
+	}
+
 	await ctx.reply(
-		subscribeMessage + `Use command /unsubscribe to remove subscription.`
+		`Subscriedb to ${hnUsername}. Use command /unsubscribe to remove subscription.`
+	)
+})
+
+bot.command('subscriptions', async (ctx: SessionContext) => {
+	const subscriptions = await getSubscriptionsByUser.run(
+		{
+			tgUserChatId: ctx.chat!.id!
+		},
+		ctx.db!
+	)
+	const userIds = subscriptions.map((s) => s.hn_user_id)
+	await ctx.reply(
+		userIds.length > 0
+			? `You're subscribed to: ${userIds.join(', ')}`
+			: `You're not subscribed to anyone.`
 	)
 })
 
 bot.command('unsubscribe', async (ctx: SessionContext) => {
-	if (!ctx.hnUsername) {
-		await ctx.reply('No active subscription found')
+	const hnUsername = getArgument(ctx, 1)
+	if (!hnUsername) {
+		await ctx.reply('Please specify username to unsubscribe from')
 		return
 	}
-	const oldUsername = ctx.hnUsername
-	ctx.hnUsername = undefined
-	await ctx.reply(`Unsubscribed from ${oldUsername}`)
+
+	const deleted = await deleteSubscription.run(
+		{ tgUserChatId: ctx.session!.chat!.id, hnUserId: hnUsername },
+		ctx.db!
+	)
+
+	await ctx.reply(
+		deleted.length > 0
+			? `Unsubscribed from ${hnUsername}. Use command /subscribe to subscribe back.`
+			: `You haven't been subscribed to ${hnUsername}. Check /subscriptions to check your active subscriptions.`
+	)
 })
 
 bot.launch()
 
+const getSubscribedUsers = sql<IGetSubscribedUsersQuery>`
+	SELECT hn_user_id
+	FROM tg_subscriptions
+	GROUP BY hn_user_id
+`
+
+/**
+ * Runs once on server start - we go through all the current users and their past post to find	comments we might have missed
+ */
 async function checkAllUsers() {
-	const users = await pg.query(`
-		SELECT
-			hn_users.hn_username AS "hnUsername",
-			hn_users.subscribed_at AS "subscribedAt",
-			"chatIds"
-		FROM hn_users,
-		LATERAL (
-			SELECT ARRAY (
-				SELECT chat_id
-				FROM tg_users
-				WHERE tg_users.hn_username = hn_users.hn_username
-			) AS "chatIds"
-		) AS tg
-		WHERE cardinality("chatIds") > 0
-	`)
-	await Promise.all(
-		users.rows.map((u) =>
-			updateUser(u.hnUsername, DateTime.fromJSDate(u.subscribedAt), u.chatIds)
-		)
-	)
+	console.log('Checking all users...')
+	const users = await getSubscribedUsers.run(undefined, pg)
+	for (const u of users) {
+		await updateUser(u.hn_user_id)
+	}
+	await markOutdatedPosts.run(undefined, pg)
+	console.log('All users checked.')
 }
 
-setInterval(checkAllUsers, 5000)
+const getUnnotifiedPosts = sql<IGetUnnotifiedPostsQuery>`
+	SELECT
+		hn_kids.id AS "itemId",
+		tg_subscriptions.tg_user_chat_id AS "chatId"
+	FROM hn_kids
+		LEFT JOIN hn_submitted ON hn_kids.parent_id = hn_submitted.id
+		INNER JOIN tg_subscriptions ON hn_submitted.hn_user_id = tg_subscriptions.hn_user_id
+	WHERE
+		NOT hn_kids.notified AND
+		hn_kids.posted_at > tg_subscriptions.subscribed_at
+`
 
-checkAllUsers()
+const setPostsNotified = sql<ISetPostsNotifiedQuery>`
+	UPDATE hn_kids
+	SET notified = TRUE
+	WHERE id IN $$ids
+`
+
+const markOutdatedPosts = sql<IMarkOutdatedPostsQuery>`
+	WITH emptyIds AS (
+		SELECT
+			hn_kids.id AS "id"
+		FROM hn_kids
+			LEFT JOIN hn_submitted ON hn_kids.parent_id = hn_submitted.id,
+			LATERAL (
+				SELECT count(tg_subscriptions.tg_user_chat_id) AS "chatIds"
+				FROM tg_subscriptions
+				WHERE
+					tg_subscriptions.hn_user_id = hn_submitted.hn_user_id AND
+					tg_subscriptions.subscribed_at < hn_kids.posted_at
+			) AS tg
+		WHERE NOT hn_kids.notified
+		GROUP BY hn_kids.id
+		HAVING sum("chatIds") = 0
+	)
+	UPDATE hn_kids
+	SET notified = TRUE
+	FROM emptyIds
+`
+
+async function loopSendNotifications() {
+	// eslint-disable-next-line no-constant-condition
+	while (true) {
+		const empty = await tx(
+			async (db): Promise<boolean> => {
+				const unnotified = await getUnnotifiedPosts.run(undefined, db)
+				if (unnotified.length === 0) {
+					return true
+				}
+				await Promise.all<unknown>([
+					setPostsNotified.run({ ids: unnotified.map((u) => u.itemId) }, db),
+					...unnotified.map(async (u) => {
+						const item = await loadHNItem(u.itemId)
+						bot.telegram.sendMessage(
+							u.chatId,
+							`You have received comment:\n${item.text}`
+						)
+					})
+				])
+				return false
+			}
+		)
+		if (empty) {
+			await promisify(setTimeout)(1000)
+		}
+	}
+}
+
+loopSendNotifications()
+
+async function checkStream() {
+	const hnStream = got.stream(
+		'https://hacker-news.firebaseio.com/v0/updates.json',
+		{
+			http2: true,
+			headers: {
+				Accept: 'text/event-stream'
+			}
+		}
+	)
+	hnStream.pipe(process.stdout)
+	hnStream.on('end', () => {
+		console.log('Stream closed')
+	})
+	/*
+	await promisify(stream.pipeline)(
+		process.stdout
+	)
+	console.log('Stream closed')
+	*/
+}
+
+;(async () => {
+	try {
+		await checkAllUsers()
+		await checkStream()
+	} catch (err) {
+		console.error('exception', err)
+		throw err
+	}
+})()
--- src/index.types.ts
+++ src/index.types.ts
@@ -29,6 +29,7 @@ export interface ICreateKidsParams {
 	kids: Array<{
 		parentId: number | null | void
 		id: number | null | void
+		postedAt: Date | null | void
 	}>
 }
 
@@ -52,7 +53,6 @@ export interface IGetSessionParams {
 /** 'GetSession' return type */
 export interface IGetSessionResult {
 	session: Json
-	hn_username: string | null
 }
 
 /** 'GetSession' query type */
@@ -65,7 +65,6 @@ export interface IGetSessionQuery {
 export interface ISetSessionParams {
 	chatId: number | null | void
 	session: Json | null | void
-	hnUsername: string | null | void
 }
 
 /** 'SetSession' return type */
@@ -84,8 +83,7 @@ export interface IGetHnUserParams {
 
 /** 'GetHnUser' return type */
 export interface IGetHnUserResult {
-	hn_username: string
-	subscribed_at: Date
+	id: string
 }
 
 /** 'GetHnUser' query type */
@@ -106,4 +104,129 @@ export type ICreateHnUserResult = void
 export interface ICreateHnUserQuery {
 	params: ICreateHnUserParams
 	result: ICreateHnUserResult
+}
+
+/** 'CreateSubscription' parameters type */
+export interface ICreateSubscriptionParams {
+	tgUserChatId: number | null | void
+	hnUserId: string | null | void
+}
+
+/** 'CreateSubscription' return type */
+export interface ICreateSubscriptionResult {
+	tg_user_chat_id: number
+	hn_user_id: string
+	subscribed_at: Date
+}
+
+/** 'CreateSubscription' query type */
+export interface ICreateSubscriptionQuery {
+	params: ICreateSubscriptionParams
+	result: ICreateSubscriptionResult
+}
+
+/** 'GetSubscriptionsByUser' parameters type */
+export interface IGetSubscriptionsByUserParams {
+	tgUserChatId: number | null | void
+}
+
+/** 'GetSubscriptionsByUser' return type */
+export interface IGetSubscriptionsByUserResult {
+	hn_user_id: string
+}
+
+/** 'GetSubscriptionsByUser' query type */
+export interface IGetSubscriptionsByUserQuery {
+	params: IGetSubscriptionsByUserParams
+	result: IGetSubscriptionsByUserResult
+}
+
+/** 'GetAllSubscriptions' parameters type */
+export type IGetAllSubscriptionsParams = void
+
+/** 'GetAllSubscriptions' return type */
+export interface IGetAllSubscriptionsResult {
+	hn_user_id: string
+	tg_user_chat_id: number
+	subscribed_at: Date
+}
+
+/** 'GetAllSubscriptions' query type */
+export interface IGetAllSubscriptionsQuery {
+	params: IGetAllSubscriptionsParams
+	result: IGetAllSubscriptionsResult
+}
+
+/** 'DeleteSubscription' parameters type */
+export interface IDeleteSubscriptionParams {
+	tgUserChatId: number | null | void
+	hnUserId: string | null | void
+}
+
+/** 'DeleteSubscription' return type */
+export interface IDeleteSubscriptionResult {
+	tg_user_chat_id: number
+	hn_user_id: string
+	subscribed_at: Date
+}
+
+/** 'DeleteSubscription' query type */
+export interface IDeleteSubscriptionQuery {
+	params: IDeleteSubscriptionParams
+	result: IDeleteSubscriptionResult
+}
+
+/** 'GetSubscribedUsers' parameters type */
+export type IGetSubscribedUsersParams = void
+
+/** 'GetSubscribedUsers' return type */
+export interface IGetSubscribedUsersResult {
+	hn_user_id: string
+}
+
+/** 'GetSubscribedUsers' query type */
+export interface IGetSubscribedUsersQuery {
+	params: IGetSubscribedUsersParams
+	result: IGetSubscribedUsersResult
+}
+
+/** 'GetUnnotifiedPosts' parameters type */
+export type IGetUnnotifiedPostsParams = void
+
+/** 'GetUnnotifiedPosts' return type */
+export interface IGetUnnotifiedPostsResult {
+	itemId: number
+	chatId: number
+}
+
+/** 'GetUnnotifiedPosts' query type */
+export interface IGetUnnotifiedPostsQuery {
+	params: IGetUnnotifiedPostsParams
+	result: IGetUnnotifiedPostsResult
+}
+
+/** 'SetPostsNotified' parameters type */
+export interface ISetPostsNotifiedParams {
+	ids: Array<number | null | void>
+}
+
+/** 'SetPostsNotified' return type */
+export type ISetPostsNotifiedResult = void
+
+/** 'SetPostsNotified' query type */
+export interface ISetPostsNotifiedQuery {
+	params: ISetPostsNotifiedParams
+	result: ISetPostsNotifiedResult
+}
+
+/** 'MarkOutdatedPosts' parameters type */
+export type IMarkOutdatedPostsParams = void
+
+/** 'MarkOutdatedPosts' return type */
+export type IMarkOutdatedPostsResult = void
+
+/** 'MarkOutdatedPosts' query type */
+export interface IMarkOutdatedPostsQuery {
+	params: IMarkOutdatedPostsParams
+	result: IMarkOutdatedPostsResult
 }
